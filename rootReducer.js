6438e1056ff5bba5f3e813609c004354976bafb8
dror <dror@lokihostel.com>
2016-03-08 20:14:10 -0500

    improved implementation objects with $type are treated differently - currently supports expire timeout

--- rootReducer.js
+++ rootReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import {counter} from './ducks/counter'
 import {clickCounter} from './ducks/clickCounter'
 import {multiplyAll} from './ducks/multiplyAll'
+import {baobabCacheReducer} from './baobab-duck'
 
 export default combineReducers({
   counter,
   clickCounter,
-  multiplyAll
+  multiplyAll,
+  baobabCacheReducer
 });
